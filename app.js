67fbbf9646b23b24e776212164726fa694bc308a
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2021-02-04 10:27:04 +0700

    final login

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+app.post("/login", (req, res) => {
+  const { email, password } = req.body
+
+  res.status(200).json({ email, password})
+})
+
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
 })
